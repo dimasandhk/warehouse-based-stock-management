ed878a59dd07196415a416e346884fafbce45acb
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2025-12-31 18:40:54 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@warehouse-based-stock-management-oppo-technical-test/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
